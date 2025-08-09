f17a0c294777b7332bb34420e1b1323ebb0b8fb2
Tonny Njoroge <antonnygichinga97@gmail.com>
2025-08-09 11:34:38 +0300

    myList adjustments

--- my.list.py
+++ my.list.py
@@ -1,1 +1,20 @@
+myList = []
+myList.append(10)
+myList.append(20)
+myList.append(30)
+myList.append(40)
 
+print(myList)
+
+myList.insert(1,15)
+
+print(myList)
+
+myList2 = [50, 60, 70]
+myList.extend(myList2)
+
+print(myList)
+
+del myList[-1]
+
+print(myList)
